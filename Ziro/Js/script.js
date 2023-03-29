8caa9882eae12e1cbe5c6f82cf3d165328768f24
doanxuanduyen <doanxuanduyen1357@gmail.com>
2023-03-29 10:09:58 +0700

    update project Ziro have slider
    update project Ziro have logo white in footer
    
    update project Ziro have logo white in footer
    
    update project Ziro have logo white in footer
    
    update project Ziro have logo white in footer

--- Ziro/Js/script.js
+++ Ziro/Js/script.js
@@ -3,6 +3,8 @@ let menuRes = document.querySelectorAll('.menu--res');
 let user = document.getElementById('user');
 let btnLogSign = document.querySelectorAll('.btn-log-sign');
 let main = document.getElementById('main');
+let scrollToTopBtn = document.getElementById("scrollUp");
+
 
 bar.addEventListener('click', function () {
     bar.classList.toggle('hidden1');
@@ -16,9 +18,34 @@ user.addEventListener('click', function () {
 
 $(document).ready(function () {
     $('.profile__cards').slick({
-        lazyLoad: 'ondemand',
-        slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToShow: 4,
+        infinite: true,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1717,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 1527,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 1063,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+        prevArrow: "<button type='button' class='slick-prev pull-left icon__slider'><i class='fa-solid fa-arrow-left aria-hidden='true'></i></button>",
+        nextArrow: "<button type='button' class='slick-next pull-right icon__slider'><i class='fa-solid fa-arrow-right aria-hidden='true'></i></i></button>"
     });
 });
 
